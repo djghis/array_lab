a98f412f785b503410de7ee0ed4ee40bc1221afc
tamaszentai <zentaitamas@gmail.com>
2020-01-15 16:27:00 +0000

    Initial commit

---
+++ my_functions.rb
@@ -0,0 +1,25 @@
+def add_array_lengths(array1, array2)
+  return array1.length()+ array2.length()
+end
+
+
+def sum_array(numbers)
+  total = 0
+  for number in numbers
+    total += number
+  end
+  return total
+end
+
+def is_item_in_array(array, item)
+  for house in array
+    if house == item
+      return true
+    end
+  end
+  return false
+end
+
+def get_first_key(hash)
+  return hash.keys[0]
+end
